1bc368bbfee9e7b8436a3fab0728cd23cd143d31
mdmehedi060 <mdmehedi060@gmail.com>
2023-11-06 22:54:28 +0600

    get added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +29,102 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const blogCollection = client.db('blogDB').collection('blog');
+    const blogAddCollection = client.db('blogAddDB').collection('blogAdd');
+    const wishlistCollection = client.db('wishlistDB').collection('wishlist');
+
+
+
+
+    // blog api
+app.get('/blog', async (req, res)=>{
+  const cursor= blogCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+app.get('/blog/:id', async (req,res)=>{
+  const id =req.params.id;
+  const quary = {_id : new ObjectId(id)};
+  const result = await blogCollection.findOne(quary);
+  res.send(result);
+})
+
+// add blog api
+
+app.get('/blogadd', async (req, res)=>{
+  const cursor= blogAddCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+
+app.post('/blogadd', async(req,res)=>{
+  const newBlog=req.body;
+  console.log(newBlog);
+  const result=await blogAddCollection.insertOne(newBlog);
+  res.send(result);
+})
+
+
+// update blog api
+
+app.get('/blogadd/:id', async (req,res)=>{
+  const id =req.params.id;
+  const quary = {_id : new ObjectId(id)};
+  const result = await blogAddCollection.findOne(quary);
+  res.send(result);
+})
+
+
+
+// app.put('/coffee/:id', async (req,res)=>{
+//   const id =req.params.id;
+//   const filter = {_id : new ObjectId(id)};
+//   const options= {upsert: true};
+//   const updatedCoffee=req.body;
+//   const coffee ={
+//     $set: {
+//       name: updatedCoffee.name, 
+//       quantity: updatedCoffee.quantity, 
+//       supplier: updatedCoffee.supplier, 
+//       test: updatedCoffee.test, 
+//       category: updatedCoffee.category, 
+//       details: updatedCoffee. details, 
+//       photo:updatedCoffee.photo,
+//     }
+//   }
+//  const result =await coffeeCollection.updateOne(filter,coffee,options);
+//  res.send(result);
+// })
+
+// wishlist api
+
+app.get('/wishlist', async (req, res)=>{
+  const cursor= wishlistCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+
+
+app.post("/wishlist", async (req, res) => {
+  const blogs = req.body;
+  const result = await wishlistCollection.insertOne(blogs);
+  console.log(result);
+  res.send(result);
+});
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
